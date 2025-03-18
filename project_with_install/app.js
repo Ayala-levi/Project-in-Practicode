636a95c74cc470200db7cb77081079a57511aa14
Ayala-levi <levihome38@gmail.com>
2025-03-18 14:21:04 +0200

    change The port

--- project_with_install/app.js
+++ project_with_install/app.js
@@ -1,17 +1,34 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 import routep from './rout.js';
 
-//http://localhost:8080/render-projects/getAll
+const app = express();
+const port = process.env.PORT || 8080;
 
-const app=express()
+app.use(cors());
 
-app.use(cors())
+app.get("/", (req, res) => {
+    res.send("Publishing a Node app");
+});
 
-app.get("/",()=>"Publishing a Node app")
+app.use('/render-projects', routep);
 
-app.use('/render-projects',routep)
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
-app.listen("8080",()=>{console.log("run barouch hashem")})
+// import express from 'express'
+// import cors from 'cors'
+// import routep from './rout.js';
 
+// //http://localhost:8080/render-projects/getAll
 
+// const app=express()
+
+// app.use(cors())
+
+// app.get("/",()=>"Publishing a Node app")
+
+// app.use('/render-projects',routep)
+
+// app.listen("8080",()=>{console.log("run barouch hashem")})
